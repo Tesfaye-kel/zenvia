183bebfbe6e6aad6fd980f93a35e31faa1418689
Tesfaye Kelbesa <tesfayekelbesa912@gmail.com>
2026-02-26 08:38:13 -0800

    meta tag improvement

--- Zenvia/register.php
+++ Zenvia/register.php
@@ -61,6 +61,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta name="author" content="TESFAYEK">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Join Zenvia | Registration</title>
     <link rel="icon" href="images/logo.png" type="image/png">

3b08778e4836791da1c4742687a798ebc92fcff9
ABISH MAN <tesfayekelbesa912@gmail.com>
2026-03-07 09:21:06 -0800

    Update captcha.php

--- Zenvia/captcha.php
+++ Zenvia/captcha.php
@@ -5,7 +5,7 @@ session_start();
 function generateCaptchaCode($length = 6) {
     $characters = 'ABCDEFGHJKLMNPQRSTUVWXYZ23456789';
     $code = '';
-    for ($i = 0; $i < $length; $i++) {
+     for ($i = 0; $i < $length; $i++) {
         $code .= $characters[rand(0, strlen($characters) - 1)];
     }
     return $code;

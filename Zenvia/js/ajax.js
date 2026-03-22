b36211a0f9f729f616991a3250d1a7088cde3f9b
ABISH MAN <tesfayekelbesa912@gmail.com>
2026-03-22 11:53:29 -0700

    Update ajax.js

--- Zenvia/js/ajax.js
+++ Zenvia/js/ajax.js
@@ -8,7 +8,7 @@ const Ajax = {
      * Make a GET request
      */
     get: function(url, callback) {
-        const xhr = new XMLHttpRequest();
+         const xhr = new XMLHttpRequest();
         xhr.open('GET', url, true);
         xhr.onreadystatechange = function() {
             if (xhr.readyState === 4) {

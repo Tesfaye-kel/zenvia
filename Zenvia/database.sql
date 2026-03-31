99f33eb91437b9f52354276ef8074dcaf96f9a48
ABISH MAN <tesfayekelbesa912@gmail.com>
2026-03-31 11:47:20 -0700

    creation SQL

--- Zenvia/database.sql
+++ Zenvia/database.sql
@@ -7,7 +7,7 @@ USE zenvia;
 -- Users table
 CREATE TABLE IF NOT EXISTS users (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(50) UNIQUE NOT NULL,
+     username VARCHAR(50) UNIQUE NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
     first_name VARCHAR(50) DEFAULT '',
